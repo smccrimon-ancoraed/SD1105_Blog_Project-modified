c91c65dea824a58bae65d6b0b47a929ab046f563
smccrimon-ancoraed <stefon.mccrimon@faculty.ancored.com>
2020-08-14 09:26:14 -0800

    Add project files.

---
+++ DailyBlogger/Controllers/BlogController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DailyBlogger.Models;
+
+
+namespace DailyBlogger.Controllers
+{
+    public class BlogController : Controller
+    {
+        private BlogContext _context;
+
+        public BlogController(BlogContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Details(int id)
+        {
+            BlogPost blogPost = _context.blogPost.Find(id);
+            return View(blogPost);
+        }
+
+        public IActionResult List()
+        {
+            IEnumerable<BlogPost> posts =   _context.blogPost.ToList<BlogPost>();
+            return View(posts);
+        }
+
+        public IActionResult New()
+        {
+            BlogPost blogPost = new BlogPost();
+
+            return View(blogPost);
+
+        }
+
+        public IActionResult Edit(int id)
+        {
+            BlogPost blogPost = _context.blogPost.Find(id);
+            return View(blogPost);
+        }
+
+
+        public IActionResult Delete([Bind("id")] int id)
+        {
+            BlogPost blogPost = _context.blogPost.Find(id);
+            _context.Remove(blogPost);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(List));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update([Bind("blogTitle,content,blogDate,id")] BlogPost blog)
+        {
+            DateTime tmpdate = DateTime.Now;  // Capture current time
+            if (ModelState.IsValid)
+            {
+                blog.blogDate = tmpdate;  // Update entry time
+                _context.Update(blog);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(List));
+            }
+            return View(blog);
+            //return RedirectToAction(nameof(List));
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("blogTitle,content,blogDate")] BlogPost blog)
+        {
+            DateTime tmpdate = DateTime.Now;  // Capture current time
+            if (ModelState.IsValid)
+            {
+                blog.blogDate = tmpdate;  // Assign entry time
+                _context.Add(blog);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(List));
+            }
+            return View(blog);
+        }
+
+    }
+}
---
+++ DailyBlogger/Migrations/20200814132908_Initial_BlogDB1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DailyBlogger.Migrations
+{
+    public partial class Initial_BlogDB1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "blogPost",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    blogTitle = table.Column<string>(nullable: true),
+                    content = table.Column<string>(nullable: true),
+                    blogDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_blogPost", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "blogPost");
+        }
+    }
+}
